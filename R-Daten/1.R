a3399cf13e2f8cf436d60a08b5a1c99041a99bb9
Philip Burggasser <cool-phil15@hotmail.com>
2018-05-28 23:22:42 +0200

    Added slider for 1st task

--- R-Daten/1.R
+++ R-Daten/1.R
@@ -9,6 +9,8 @@ ui <- fluidPage(
                 "Education",
                 "Catholic",
                 "Infant")),
+  sliderInput("bw_adjust", label = "Bandwidth adjustment:",
+              min = 0.2, max = 2, value = 1, step = 0.2),
   plotOutput("hist"),
   verbatimTextOutput("stats")
 )
@@ -22,10 +24,14 @@ server <- function(input, output) {
            "Catholic" = swiss$Catholic,
            "Infant" = swiss$Infant.Mortality)
   })
+  
   output$hist <- renderPlot({
-    hist(datasetInput(),
+    hist(datasetInput(), probability = T,
     main = input$cmbData)
+    dens <- density(datasetInput(), adjust = input$bw_adjust)
+    lines(dens, col = "blue")
   })
+  
   output$stats <- renderPrint({
     summary(datasetInput())
   })
