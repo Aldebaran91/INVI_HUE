37239de31f403878ff92855e6bdf1ae846e742b5
adelinateofanescu <adelinateofanescu@yahoo.com>
2018-06-18 22:53:19 +0200

    Scatterplots hinzugefügt

--- R-Daten/2.R
+++ R-Daten/2.R
@@ -4,10 +4,10 @@ library(psych)
 library(moments)
 
 ui <- fluidPage(
- 
-    tabsetPanel(id = "tabselected",
-                tabPanel("Dataset Summary",
-                         sidebarLayout(
+  
+  tabsetPanel(id = "tabselected",
+              tabPanel("Dataset Summary",
+                       sidebarLayout(
                          sidebarPanel(
                            radioButtons(inputId = "cmbData",
                                         label = "Choose a dataset:", 
@@ -34,15 +34,34 @@ ui <- fluidPage(
                                        value = 20)
                          ),
                          mainPanel(
-                         
-                         helpText("Summary"), verbatimTextOutput("summary"),
-                         helpText("Expectation"), verbatimTextOutput("expectation"), 
-                         helpText("Variance"), verbatimTextOutput("variance"), 
-                         helpText("Skewness"), verbatimTextOutput("skewness"), 
-                         helpText("Kurtosis"),verbatimTextOutput("kurtosis"), plotOutput("out")))),
-                tabPanel("Regression", plotOutput("pairs"),plotOutput("reg1"),plotOutput("reg2"),plotOutput("reg3"),plotOutput("reg5"))
-                )
+                           
+                           helpText("Summary"), verbatimTextOutput("summary"),
+                           helpText("Expectation"), verbatimTextOutput("expectation"), 
+                           helpText("Variance"), verbatimTextOutput("variance"), 
+                           helpText("Skewness"), verbatimTextOutput("skewness"), 
+                           helpText("Kurtosis"),verbatimTextOutput("kurtosis"), plotOutput("out")))),
+              tabPanel("Regression", plotOutput("pairs"),plotOutput("reg1"),plotOutput("reg2"),plotOutput("reg3"),plotOutput("reg5")),
+              tabPanel("Scatterplots", 
+                       sidebarLayout(
+                         sidebarPanel(
+                           radioButtons(inputId = "cmbData1",
+                                        label = "Choose a dataset:", 
+                                        choices = c(
+                                          "Population",
+                                          "Income")),
+                           radioButtons(inputId = "cmbData2",
+                                        label = "Choose a dataset:", 
+                                        choices = c(
+                                          "Murder",
+                                          "Illiteracy",
+                                          "HS.Grad",
+                                          "Life.Exp",
+                                          "Frost"))
+                         ),
+                       mainPanel(plotOutput("scatterplot"))
+                       ))
   )
+)
 
 
 
@@ -147,6 +166,16 @@ server <- function(input, output) {
   })
   output$skewness <- renderPrint({
     skewness(s[,input$cmbData])
+  })
+  
+  output$scatterplot <- renderPlot({
+    # Render a scatterplot
+    plot(log(s[,input$cmbData1])~s[,input$cmbData2],
+         
+         main=paste("Scatterplot over all States with the variables", input$cmbData1," and ", input$cmbData2),
+         ylab=input$cmbData1,
+         xlab=input$cmbData2)
+    abline(lm(log(s[,input$cmbData1])~s[,input$cmbData2]))
   })
 }
 
